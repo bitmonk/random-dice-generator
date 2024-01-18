2db94d8c0a1925110fe6a66c55102426bd0bf563
RyanLama <parbattamang1234@gmail.com>
2024-01-18 12:54:37 +0545

    js changes

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="https://cdn.rawgit.com/fponticelli/dicefont/fc6b8c47/dist/dicefont/dicefont.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.cdnfonts.com/css/marvin" rel="stylesheet">
+                
+
 </head>
 <body>
     <img src="./images/logo.png" class="logo-image" alt="404">
@@ -40,18 +43,8 @@
 
 <div class="roll-area">
 
-<div class="dice dice-one"><i class="fa-solid fa-line-height-inherit fa-dice-two fa-5x icon"></i></div>
-<div class="dice dice-two"><i class="fa-solid fa-dice-two fa-5x"></i></div>
-<div class="dice dice-three"><i class="fa-solid fa-dice-three fa-5x"></i></div>
-<div class="dice dice-four"><i class="fa-solid fa-dice-four fa-5x"></i></div>
-<div class="dice dice-five"><i class="fa-solid fa-dice-five fa-5x"></i></div>
-<div class="dice dice-six"><i class="fa-solid fa-dice-six fa-5x"></i></div>
-<div class="dice dice-six"><i class="fa-solid fa-dice-six fa-5x"></i></div>
-<div class="dice dice-six"><i class="fa-solid fa-dice-six fa-5x"></i></div>
-<div class="dice dice-six"><i class="fa-solid fa-dice-six fa-5x"></i></div>
-<div class="dice dice-six"><i class="fa-solid fa-dice-six fa-5x"></i></div>
-<div class="dice dice-six"><i class="fa-solid fa-dice-six fa-5x"></i></div>
-<div class="dice dice-six"><i class="fa-solid fa-dice-six fa-5x"></i></div>
+<!-- <div class="dice dice-one"><i class="fa-solid fa-dice-two fa-5x icon"></i></div>
+<div class="dice dice-two"><i class="fa-solid fa-dice-two fa-5x"></i></div> -->
 
 
     </div>
@@ -62,45 +55,53 @@
           <input class="roll-button" type="button" value="ROLL !" onclick="rollDice()">  
     </div>
          </div>
-  
                     </div>
-
-
 </div>
 
 
 
 
 <script>
-            function rollDice() {
-                var numDice = document.getElementById('dice-amount').value;
-                var numSides = document.getElementById('sided').value;
+    function rollDice() {
+        var numDice = document.getElementById('dice-amount').value;
+        var numSides = document.getElementById('sided').value;
 
-                // var rollArea = document.querySelector('.roll-area');
-                // rollArea.innerHTML = ''; // Clear previous dice icon
+        var rollArea = document.querySelector('.roll-area');
+        rollArea.innerHTML = ''; // Clear previous dice icons
 
-                if (numSides === 'colour') {
-                    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16)
+        var results = [];
+        var totalResult = 0;
 
+        for (let i = 0; i < numDice; i++) {
+            var diceResult = Math.floor(Math.random() * numSides) + 1;
+            results.push(diceResult);
+            totalResult += diceResult;
 
-                    document.getElementById('result').innerHTML = 'Result: <span style="color:' + randomColor + ';">' + randomColor + '</span>';
-                } else {
-                    var results = [];
-                    var totalResult = 0;
-                    for (var i = 0; i < numDice; i++) {
-                        var dieResult = Math.floor(Math.random() * numSides) + 1;
-                        results.push(dieResult);
-                        totalResult += dieResult;
-                    }
+            console.log(diceResult);
 
-                    
-                    document.getElementById('result').innerText = results.join(' ');
-                    document.getElementById('result').innerHTML += '<br>Result: ' + totalResult;
-                }
-            }
-        </script>
+            var diceDiv = document.createElement("div");
+            diceDiv.className = "dice dice-" + diceResult;
+
+            var diceIcon = document.createElement("i");
+            diceIcon.className = "fa-solid fa-dice-two fa-5x icon";
+
+            diceDiv.appendChild(diceIcon);
+            rollArea.appendChild(diceDiv);
+        }
+
+        if (numSides === 'colour') {
+            var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+            document.getElementById('result').innerHTML = 'Result: <span style="color:' + randomColor + ';">' + randomColor + '</span>';
+        } else {
+            document.getElementById('result').innerText = results.join(' ');
+            document.getElementById('result').innerHTML += '<br>Result: ' + totalResult;
+        }
+    }
+</script>
 
 
 <script src="https://kit.fontawesome.com/e8b54f58bf.js" crossorigin="anonymous"></script>
+<script src="app.js"></script>
 </body>
 </html>
