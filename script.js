ff9380dc17e6cdd5685f50905b5b925ef3f0498c
RyanLama <parbattamang1234@gmail.com>
2024-01-18 17:28:32 +0545

    js touches

--- script.js
+++ script.js
@@ -24,10 +24,11 @@ function rollDice() {
     
             totalResult += randomIndex + 1;
         }
-
+    
         var resultArea = document.getElementById('result');
         resultArea.textContent = 'RESULT: ' + totalResult;
     } else if (numSides === '6') {
+        
         for (let i = 0; i < numDice; i++) {
             const diceClasses = ['dice-one', 'dice-two', 'dice-three', 'dice-four', 'dice-five', 'dice-six'];
             const randomIndex = Math.floor(Math.random() * diceClasses.length);
@@ -42,7 +43,10 @@ function rollDice() {
             fourSidedArea.appendChild(diceDiv);
     
             totalResult += randomIndex + 1;
+            diceColor = document.getElementsByClassName('dice');
+        diceColor.style.color = '#ffffff';
         }
+
         var resultArea = document.getElementById('result');
         resultArea.textContent = 'RESULT: ' + totalResult;
     } else if (numSides === '8') {
@@ -51,8 +55,28 @@ function rollDice() {
     } else if (numSides === 'colour') {
         var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-        document.getElementById('result').innerHTML = 'Result: <span style="color:' + randomColor + ';">' + randomColor + '</span>';
+        for (let i = 0; i < numDice; i++) {
+            const diceClasses = ['dice-one'];
+            const randomIndex = Math.floor(Math.random() * diceClasses.length);
+    
+            var diceDiv = document.createElement("div");
+            diceDiv.className = "dice " + diceClasses[randomIndex];
+    
+            var diceIcon = document.createElement("i");
+            diceIcon.className = "fa-solid fa-" + diceClasses[randomIndex] + " fa-5x icon";
+    
+            diceDiv.appendChild(diceIcon);
+            fourSidedArea.appendChild(diceDiv);
+    
+            totalResult += randomIndex + 1;
+        }
+
+        // Set the color for all the dice elements
+        var colorDivs = document.getElementsByClassName('dice');
+        for (let j = 0; j < colorDivs.length; j++) {
+            colorDivs[j].style.color = randomColor;
+        }
     } else {
-        document.getElementById('result').innerHTML = 'Result: ' + totalResult;
+        document.getElementById('result').innerHTML = 'Result:' + totalResult;
     }
 }
